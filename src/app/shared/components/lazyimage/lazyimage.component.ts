91494fca7feafd425f5523867751e0b790b0936a
jotaulian <julianeggle@hotmail.com>
2024-10-08 12:18:05 +0200

    gifs app Angular

---
+++ src/app/shared/components/lazyimage/lazyimage.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazyimage',
+  templateUrl: './lazyimage.component.html',
+})
+export class LazyimageComponent implements OnInit{
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required.');
+  }
+
+  onLoad(){
+    console.log('Image loaded');
+    setTimeout(() => {
+      this.hasLoaded = true;
+
+    }, 500);
+  }
+}
